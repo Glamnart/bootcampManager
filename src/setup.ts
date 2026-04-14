e0895e1f9dd065102c0a25a3515afb7ce4c6abc3
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2026-04-14 14:17:35 +0100

    this sets up the Project setup

---
+++ src/setup.ts
@@ -0,0 +1,8 @@
+export type Coordinate = [number, number];
+export type ScoreCalculator = (scores: number[]) => number;
+ const bootcampName: string = "";
+const currentWeek: number = 0;
+ const classIsLive: boolean = false;
+ const availableTracks: string[] = [];
+export type StudentId = string | number;
+
